50245d4cf89fbb9cfbe699b4877d427917243a17
Malshani RathnaSri <malshanirathnasri@gmailcom>
2025-09-10 01:28:31 +0530

    Finalize 2

--- assignment/src/app/page.tsx
+++ assignment/src/app/page.tsx
@@ -4,10 +4,19 @@ import BrokerOverview from "@/components/BrokerOverview";
 
 export default function Page() {
   return (
-    <main className="dark bg-circles grid grid-cols-1 md:grid-cols-3 gap-4 p-4 relative z-10">
-      <BorrowerPipeline />
-      <BorrowerDetail />
-      <BrokerOverview />
-    </main>
+    <body className="dark">
+  <div className="bg-circles">
+    <div className="circle1"></div>
+    <div className="circle2"></div>
+    <div className="circle3"></div>
+  </div>
+
+  <main className="relative z-10 grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+    <BorrowerPipeline />
+    <BorrowerDetail />
+    <BrokerOverview />
+  </main>
+</body>
+
   );
 }
